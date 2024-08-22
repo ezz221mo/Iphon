978dbab3901e916730710ca5d194ffbb69a629f0
ezz221mo <150435494+ezz221mo@users.noreply.github.com>
2024-08-22 15:35:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.image');
+let continar = document.querySelector('.continar');
+
+function phones(phone){
+    img.src = phone;
+}
+
+function colors(color){
+    continar.style.background = color;
+}
